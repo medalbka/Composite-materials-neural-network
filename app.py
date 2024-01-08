377a02f2706c2df0ea0e33884499b8d34bd3525d
Edok teftelei <rrooll@mail.ru>
2024-01-08 17:39:31 +0300

    test1

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import flask
 from flask import render_template
-import pickle
 import sklearn
-from sklearn.linear_model import LinearRegression
+import numpy as np
+import pandas as pd
+import tensorflow
+import random
 
 app = flask.Flask(__name__, template_folder = 'templates')
 
@@ -14,13 +16,21 @@ def main():
     if flask.request.method == 'GET':
         return render_template('main.html')
 
+
     if flask.request.method == 'POST':
-        with open('lr_model.pkl', 'rb') as f:
-            loaded_model = pickle.load(f)
+        loaded_model = tensorflow.keras.models.load_model('my_model.h5')
+        total = []
 
-        exp = float(flask.request.form['experience'])
-        y_pred = loaded_model.predict([[exp]])
-
+        if flask.request.form['submit_button'] == 'Прогноз':
+            parametrs = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'A11', 'A12']
+            for number in parametrs:
+                total.append(float(flask.request.form[number]))
+        else:
+            #X_test = pd.read_csv('X_test.csv').to_numpy(dtype = 'float')
+            #total = random.choice(X_test)
+            total = [random.uniform(0, 2000) for _ in range(12)]
+        total = np.array(total).reshape(1, 12)    
+        y_pred = loaded_model.predict(total)[0][0]
         return render_template('main.html', result = y_pred)
 
 if __name__ == '__main__':
